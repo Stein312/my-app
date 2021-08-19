19315b020da1f5bf7215e6551a42230a00fd0374
Damba <dbovaev@softailor.ru>
2021-08-19 10:18:52 +0300

    Create table

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import 'antd/dist/antd.css';
-import {Button} from "antd";
+import { Table } from "antd";
+import {ColumnsType} from "antd/lib/table/interface";
 
+interface columnType{
+  title: string,
+  width: number,
+  dataIndex: string,
+  key: string,
+  fixed: string,
+}
+const columns:ColumnsType<columnType> = [
+  {
+    title: 'Full Name',
+    width: 100,
+    dataIndex: 'name',
+    key: 'name',
+    fixed: 'left',
+  },
+  {
+    title: 'Age',
+    width: 100,
+    dataIndex: 'age',
+    key: 'age',
+    fixed: 'left',
+  },
+  {
+    title: 'Column 1',
+    dataIndex: 'address',
+    key: '1',
+    width: 150,
+  },
+];
+
+
+const data:any[]=[];
+for (let i = 0; i < 20; i++) {
+  data.push({
+    key: i,
+    name: `Edward. ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
 function App() {
+  // @ts-ignore
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button>OK</Button>
+      <Table columns={columns} dataSource={data} scroll={{ x: 1500, y: 400 }} />,
     </div>
   );
 }
